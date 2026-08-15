06b2dbd73fbd7c145786ab2733369dd5d215695f
Muzahidul Haque Jaber <mhjaber519@gmail.com>
2026-08-15 23:11:32 +0600

    Update project

---
+++ Chapter 2/question1.js
@@ -0,0 +1,5 @@
+const name = "Muzahidul Haque Jaber";
+const department = "Computer Science and Engineering";
+let semester = "3rd Year";
+
+console.log(`My name is ${name}. I am studying in the ${department} department, and I am currently in ${semester}.`);
---
+++ question3.js
@@ -0,0 +1,6 @@
+const str = "19.5";
+const number = parseFloat(str);
+
+const result = number + 0.5;
+
+console.log(result);
